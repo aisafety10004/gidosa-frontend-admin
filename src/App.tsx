de823505e8bcf93d4f30d545270b14b62149d895
pktjesus1 <pktjesus@gmail.com>
2025-02-11 23:51:33 +0900

    2차 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import "@/styles/globals.css";
-import './App.css'
+import React from 'react';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import Layout from './components/Layout';
+// import Home from './pages/Home';
+// import Register from './pages/member/Register';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background: #c1c1c1;
+    border-radius: 5px;
+  }
+  
+  ::-webkit-scrollbar-thumb:hover {
+    background: #a8a8a8;
+  }
+`;
+
+const queryClient = new QueryClient();
+
+const App: React.FC = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <GlobalStyle />
+        <Layout>
+          <Routes>
+            {/* <Route path="/" element={<Navigate to="/dashboard" replace />} /> */}
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="/register" element={<Register />} /> */}
+          </Routes>
+        </Layout>
+      </Router>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
